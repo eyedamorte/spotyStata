a541629548658f1b7886516fec4ff3d72fdf6fb3
Eyedamorte <30478301+eyedamorte@users.noreply.github.com>
2021-07-24 19:53:54 +0000

    удалил лишнее
    удалил лишнее

--- frontend/src/features/filters/filtersTypes.ts
+++
@@ -1,4 +0,0 @@
-export type FiltersStateType = {
-    search: string,
-    isTopHeadlines: boolean
-}
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,13 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import filtersReducer from './features/filters/FiltersSlice'
-import newsReducer from './features/news/NewsSlice'
 import artistsReducer from './features/artists/ArtistsSlice'
 import releasesReducer from './features/releases/ReleasesSlice'
 
 
 const RootReducer = combineReducers({
-    filters: filtersReducer,
-    news: newsReducer,
     artists: artistsReducer,
     releases: releasesReducer
 })
